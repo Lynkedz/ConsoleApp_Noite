eb5ede504a0d7413e57b66115a09c1897c11c702
Vitor <lynkedvd@gmail.com>
2023-04-03 00:04:42 -0300

    Lista venda

--- ConsoleApp_Noite/Program.cs
+++ ConsoleApp_Noite/Program.cs
@@ -4,7 +4,7 @@ using ConsoleApp_Noite.Heranca;
 /*Produto p1 = new Produto(1, 10.90M, "Cervejinha");
 
 p1.setValor(11.85M);
-p1.setDescricao("Cerveja Skol 1 litro
+p1.setDescricao("Cerveja Skol 1 litro");
 
 Console.WriteLine($"{p1.getDescricao()} {p1.getId()} R${p1.getValor()}");
 
@@ -16,8 +16,71 @@ Cliente c3 = new Cliente(3, "Teste 3" , "856213403", endereco);
 Cliente c4 = new Cliente(4, "Teste 4" , "856213404", endereco);
 Cliente c5 = new Cliente(5, "Teste 5" , "856213405", endereco);*/
 
+Produto p1 = new Produto(1, 10.90M, "Cervejinha");
+p1.setId(1);
+p1.setValor(10.90M);
+p1.setDescricao("Cerveja Skol 1 litro");
 
-Carro gol = new Carro();
+Produto p2 = new Produto(2, 50.00M, "Carne");
+p2.setId(2);
+p2.setValor(50.00M);
+p2.setDescricao("1Kg Carne");
+
+Produto p3 = new Produto(3, 8.90M, "Arroz");
+p3.setId(3);
+p3.setValor(8.90M);
+p3.setDescricao("Arroz Tio João 1kg");
+
+Produto p4 = new Produto(4, 2.50M, "Salgadinho");
+p4.setId(4);
+p4.setValor(2.50M);
+p4.setDescricao("Doritos 50g");
+
+Produto p5 = new Produto(5, 5.00M, "Refrigerante");
+p5.setId(5);
+p5.setValor(5.00M);
+p5.setDescricao("Coca-cola 2L");
+
+Produto p6 = new Produto(6, 3.00M, "Feijão");
+p6.setId(6);
+p6.setValor(3.00M);
+p6.setDescricao("Feijão Carioca 1kg");
+
+Produto p7 = new Produto(7, 12.00M, "Picanha");
+p7.setId(7);
+p7.setValor(12.00M);
+p7.setDescricao("Picanha Angus 1kg");
+
+Produto p8 = new Produto(8, 1.50M, "Pão francês");
+p8.setId(8);
+p8.setValor(1.50M);
+p8.setDescricao("Pão francês da padaria do seu Zé");
+
+Produto p9 = new Produto(9, 7.90M, "Queijo");
+p9.setId(9);
+p9.setValor(7.90M);
+p9.setDescricao("Queijo Minas frescal 500g");
+
+Produto p10 = new Produto(10, 6.00M, "Macarrão");
+p10.setId(10);
+p10.setValor(6.00M);
+p10.setDescricao("Macarrão espaguete Barilla 500g");
+
+
+List<Produto> listaProduto = new List<Produto>();
+listaProduto.Add(p1);
+listaProduto.Add(p2);
+listaProduto.Add(p3);
+listaProduto.Add(p4);
+listaProduto.Add(p5);
+listaProduto.Add(p6);
+listaProduto.Add(p7);
+listaProduto.Add(p8);
+listaProduto.Add(p9);
+listaProduto.Add(p10);
+
+
+/*Carro gol = new Carro();
 gol.numeroAssentos = 5;
 gol.temMotor = true;
 gol.numeroPortas = 4;
@@ -66,7 +129,7 @@ mountain.temMotor = false;
 mountain.numeroPortas  = 0;
 mountain.numeroAssentos = 1;
 
-/*Console.WriteLine(monark.ExibirDados());
+Console.WriteLine(monark.ExibirDados());
 Console.WriteLine(gol.ExibirDados());
 
 List<Carro> listaCarro = new List<Carro>();
@@ -84,7 +147,7 @@ for (int i = 0; i < listaCarro.Count; i++)
 foreach (var item in listaCarro)
 {
     Console.WriteLine($"{item.ExibirMarca()}");
-}*/
+}
 
 //Imprima na tela todos os clientes desse contexto
 //Crie uma lista com 560 produtos
@@ -116,4 +179,16 @@ for (int i = 0; i <= 50; i++)
 }
 
 foreach (var cliente in listaCliente)
-    Console.WriteLine($"{cliente.ExibirInformacao()}");
+    Console.WriteLine($"{cliente.ExibirInformacao()}");*/
+
+Venda venda1 = new Venda(1, "01/04/2023", listaProduto);
+
+List<Venda> listaVenda = new List<Venda>();
+listaVenda.Add(venda1);
+
+foreach (var venda in listaVenda)
+    Console.WriteLine(venda.ImprimirDados());
+
+foreach (var produto in listaProduto)
+    Console.WriteLine(produto.getId() +"\t" + produto.getValor() + "\t" + produto.getDescricao() + "\n");
+//a lista aparece como System.Collections.Generic.List`1[ConsoleApp_Noite.Produto] 
---
+++ ConsoleApp_Noite/Venda.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Noite
+{
+    public class Venda
+    {
+        private int Id;
+        private string Data;
+        private List<Produto> Produtos;
+
+        public Venda(int id, string data, List<Produto> produtos)
+        {
+            Id = id;
+            Data = data;
+            Produtos = produtos;
+        }
+
+        public void setId(int id)
+        {
+            Id = id;
+        }
+        public int getId()
+        {
+            return Id;
+        }
+        public string ImprimirDados()
+        {
+            return $"Id da venda: {Id} Data da venda: {Data} Produtos Comprados:";
+            
+        } 
+    }
+
+}
